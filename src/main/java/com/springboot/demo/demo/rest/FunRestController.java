7a487adb1f1e5fc5d4141d06089991c16f8f701d
Ashraf Shaik <ashraf.shaik@zentrades.pro>
2025-05-15 20:46:15 +0530

    rest controller added

---
+++ src/main/java/com/springboot/demo/demo/rest/FunRestController.java
@@ -0,0 +1,15 @@
+package com.springboot.demo.demo.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+    
+}
