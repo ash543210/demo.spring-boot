67b2865f78030da829f17f204f84055c918223d6
ash543210 <shaikashraf54321@gmail.com>
2025-05-19 11:23:31 +0530

    some more learning

--- src/main/java/com/springboot/demo/demo/rest/FunRestController.java
+++ src/main/java/com/springboot/demo/demo/rest/FunRestController.java
@@ -3,13 +3,22 @@ package com.springboot.demo.demo.rest;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @RestController
 public class FunRestController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World!";
+        return "Hello World!!!!";
     }
-    
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day!";
+    }
+
 }
